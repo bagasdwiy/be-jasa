a81aeacd4ecbf6ec4b49153054c0478340f97b2d
bagasdwiy <bagasdwi214@gmail.com>
2023-12-02 00:46:54 +0700

    be-jasa

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const { Register } = require('../controllers/register')
 const { Login } = require('../controllers/login')
+const { handleRoot } = require('../controllers/root')
 
 const router = express.Router()
-
+router.get('/', handleRoot)
 router.post('/register', Register )
 router.post('/login', Login )
 module.exports = router
